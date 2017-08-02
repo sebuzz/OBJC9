96e60606843c13b34d7cc060156493022f2393ee
sebuzz <sebuzz@mac.com>
2017-08-02 20:43:49 +0200

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bildbetrachter
+//
+//  Created by Sebastian Künne on 02.08.17.
+//  Copyright © 2017 Sebastian Künne. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
